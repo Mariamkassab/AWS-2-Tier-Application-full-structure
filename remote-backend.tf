f25cea08c959f5592424ce81c49187a4990eeae7
Mariam Kassab <mariam.solimankassab@gmail.com>
2023-11-03 23:07:01 +0200

    latest

--- remote-backend.tf
+++ remote-backend.tf
@@ -1,26 +1,38 @@
 
-# resource "aws_s3_bucket" "terraform-state" {
-#   bucket = "terraform-statefile-mariam"
-#   lifecycle {
-#     #    prevent_destroy = true
-#   }
-# }
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "terraform-statefile-mariam"
+  lifecycle {
+    #    prevent_destroy = true
+  }
+}
 
 
 
-# resource "aws_s3_bucket_versioning" "enabled" {
-#   bucket = aws_s3_bucket.terraform-state.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform-state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform-lockstate" {
+  name         = "terraform-lockstate"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
 
 
-# terraform {
-#   backend "s3" {
-#     bucket         = "terraform-statefile-mariam"
-#     key            = "terraform.tfstate"
-#     region         = "eu-west-1"
-#     encrypt        = "true"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "terraform-statefile-mariam"
+    key            = "terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-lockstate"
+    encrypt        = "true"
+  }
+}
